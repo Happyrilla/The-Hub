7f813cac38c19e0525eb7fbe64707cc38ad0af20
happyrilla <basukabomberumpe@gmail.com>
2026-05-18 09:01:25 +0200

    better code and two more facts

--- js/customizer.js
+++ js/customizer.js
@@ -4,5 +4,5 @@ if (idk === null) {
     setTitle("Google")
 } else {
     setTitle(idk);
-    console.log("the title set is:", idk, ". you can change it in the settings");
+    console.log("the title set is:", idk, "you can change it in the settings");
 }
