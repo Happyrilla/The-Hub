1fcee3f739f0a66bc96c63f9f502937e1fd61184
happyrilla <basukabomberumpe@gmail.com>
2026-05-19 13:18:43 +0200

    inject js

---
+++ js/inject.js
@@ -0,0 +1,9 @@
+const i3 = document.getElementById("3");
+const i4 = document.getElementById("4");
+
+const l3 = localStorage.getItem("panicbutton");
+const l4 = localStorage.getItem("paniclink");
+
+
+i3.innertext = l3
+i4.innertext = l4
