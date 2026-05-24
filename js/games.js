5e4564055ff71f12e6f791245b945e097317dd27
happyrilla <basukabomberumpe@gmail.com>
2026-05-24 22:44:54 +0200

    start

---
+++ js/games.js
@@ -0,0 +1,10 @@
+function gamer(value) {
+    fetch("/api/test")
+    .then(res => res.json())
+    .then(data => {
+    if (value === data.value) {
+        window.location.href = "../sub-sites/devdash.html";
+    } else {
+        localStorage.setItem("iscool?", "yes")
+    }
+  });};
