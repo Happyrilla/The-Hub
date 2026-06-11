ec5fc0becc3746c289faa6b2d252d420d51529e9
happyrilla <basukabomberumpe@gmail.com>
2026-06-11 07:29:33 +0000

    fix

--- js/customizer.js
+++ js/customizer.js
@@ -1,15 +1,17 @@
-let idk = localStorage.getItem("title")
-let favi = localStorage.getItem("favicon")
-function setTitle(value) {document.title = value; localStorage.setItem("title", value)}
-function setfavicon(icon) {document.querySelector("link[rel*='icon']").href = icon; localStorage.setItem("favicon", icon)}
-if (idk === null) {
-    setTitle("Google")
-} else {
-    setTitle(idk);
-    console.log("the title set is:", idk, "you can change it in the settings");
-}
-if (favi === null) {
-    setfavicon("https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico");
-} else {
-    setfavicon(favi)
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+    let idk = localStorage.getItem("title")
+    let favi = localStorage.getItem("favicon")
+    function setTitle(value) { document.title = value; localStorage.setItem("title", value) }
+    function setfavicon(icon) { document.querySelector("link[rel*='icon']").href = icon; localStorage.setItem("favicon", icon) }
+    if (idk === null) {
+        setTitle("Google")
+    } else {
+        setTitle(idk);
+        console.log("the title set is:", idk, "you can change it in the settings");
+    }
+    if (favi === null) {
+        setfavicon("https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico");
+    } else {
+        setfavicon(favi)
+    }
+})
