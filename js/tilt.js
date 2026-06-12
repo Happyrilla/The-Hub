603d1b33e8675b4b8e5a91911c98f391fd66d567
happyrilla <basukabomberumpe@gmail.com>
2026-06-12 19:33:30 +0200

    test

--- js/tilt.js
+++ js/tilt.js
@@ -421,7 +421,7 @@ class VanillaTilt {
   extendSettings(settings) {
     let defaultSettings = {
       reverse: false,
-      max: 15,
+      max: 100,
       startX: 0,
       startY: 0,
       perspective: 1000,
