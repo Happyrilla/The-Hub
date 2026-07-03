23d26d8717dd27853cb6f21fb85257b2b8d06049
Happyrilla <basukabomberumpe@gmail.com>
2026-07-03 10:40:03 +0200

    so much stuff

---
+++ js/injector.js
@@ -0,0 +1,6 @@
+const thing = document.getElementById('inject');
+
+    EJS_player = "#inject";
+    EJS_gameUrl = thing.dataset.game;
+    EJS_core = thing.dataset.core;
+    EJS_pathtodata = "../stable/data/";
